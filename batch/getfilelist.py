00e0f57773dd90e68125951e80353c21d4b57f82
Ashraf Kasem <ashraf.mohamed@cern.ch>
2018-04-13 15:57:57 +0200

    fixing run time issue for Run C,E,G data,add list of 2016 data, polish the submit batch script for a bit

--- batch/getfilelist.py
+++ batch/getfilelist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ########### author : Ashraf Kasem Mohamed ########## 
 ########### institute : DESY #######################
 ########### Email : ashraf.mohamed@desy.de #########
@@ -13,7 +14,8 @@ import shutil
 ## to be changed by user 
 workarea = '/nfs/dust/cms/user/amohamed/susy-desy/nanoAOD/pureNANOAOD/CMSSW_9_4_4/src'
 exearea = '/nfs/dust/cms/user/amohamed/susy-desy/nanoAOD/pureNANOAOD/CMSSW_9_4_4/src/tthAnalysis/NanoAODTools/batch'
-X509 = 'x509up_u29118'
+X509 = '/nfs/dust/cms/user/amohamed/susy-desy/nanoAOD/pureNANOAOD/CMSSW_9_4_4/src/tthAnalysis/NanoAODTools/batch/x509up_u29118'
+#X509name = 'x509up_u29118'
 ## to be kept 
 condTEMP = './templates/submit.condor'
 wrapTEMP = './templates/wrapnanoPost.sh'
@@ -42,23 +44,23 @@ def moduletorun(sample):
 	isUSER = False 
 	era = 'unknown'
 	modu = 'unknown'
-	if 'RunIISummer16NanoAOD' in str(sample) : era = 2016
-	elif 'RunIIFall17NanoAOD' in str(sample) : era = 2016
+	if 'RunIISummer16NanoAOD' in str(sample) or "Run2016" in str(sample): era = 2016
+	elif 'RunIIFall17NanoAOD' in str(sample) or "Run2017" in str(sample): era = 2017
 	if '/NANOAODSIM' in sample:
 		isMC = True 
 	if "/SMS-T1tttt" in sample:
 		isSIG = True
 	if isMC and not isSIG : 
-		modu = "susy1lepMC,susy_1l_Trigg,susy_1l_FiltersMC,btagSF_csvv2,btagSF_deep,jecUncert,puWeight"#,xsec,genpartsusymod
+		modu = "susy1lepMC,susy_1l_Trigg,susy_1l_FiltersMC,btagSF_csvv2,btagSF_deep,jecUncert,puWeight,eventCountHistogram"#,xsec,genpartsusymod
 		if era == 2016 : modu+="jetmetUncertainties16"
 		if era == 2017 : modu+="jetmetUncertainties17"
 	elif isMC and isSIG :
-		modu = "susy1lepSIG,susy_1l_Trigg,susy_1l_FiltersMC,btagSF_csvv2,btagSF_deep,jecUncert,puWeight"#,xsec,genpartsusymod
+		modu = "susy1lepSIG,susy_1l_Trigg,susy_1l_FiltersMC,btagSF_csvv2,btagSF_deep,jecUncert,puWeight,eventCountHistogram"#,xsec,genpartsusymod
 		if era == 2016 : modu+="jetmetUncertainties16"
 		if era == 2017 : modu+="jetmetUncertainties17"
 	else : 
 		modu = "susy1lepdata,susy_1l_Trigg,susy_1l_FiltersData"
-	return modu 
+	return modu
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Runs a NAF batch system for nanoAOD', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -69,14 +71,19 @@ if __name__ == '__main__':
 	
 	outdire = args.out
 	
+	os.system('export X509_USER_PROXY='+X509)
+	os.system('voms-proxy-init --voms cms --valid 60:00')
+	
+
+		
 	if  os.path.exists(outdire):
 		des = raw_input(" this dir is already exist : "+str(outdire)+" do you want to remove it [y/n]: ")
-		if (des == "y" or des =="Y" or des =="Yes") : 
+		if ( "y" in des or "Y" in des or "Yes" in des) : 
 			shutil.rmtree(str(outdire))
 			os.makedirs(str(outdire))
-		elif (des == "N" or des == "n" or des == "No"): print str(outdire) , "will be ovewritten by the job output -- take care"  
+		elif ( "N" in des or  "n" in des or  "No" in des ): print str(outdire) , "will be ovewritten by the job output -- take care"  
 		else :
-			print "do not understand your potion"
+			raise ValueError( "do not understand your potion")
 	else : os.makedirs(str(outdire))
 	flist = []
 	modulelist = [] 
@@ -110,7 +117,8 @@ if __name__ == '__main__':
 				shutil.rmtree(str(dirname))
 				os.makedirs(str(dirname))
 			elif (des == "N" or des == "n" or des == "No"): print str(dirname) , "will be ovewritten by the job output -- take care" ; continue 
-			else : print "do not understand your potion"; break
+			else : raise ValueError( "do not understand your potion")
+			
 		else :os.makedirs(str(dirname))
 		file = open(dirname+'/'+textname+'.txt',"w+")
 		for f in flist : 
@@ -120,11 +128,15 @@ if __name__ == '__main__':
 		logsdir = dirname+"/logs"
 		os.makedirs(logsdir)
 		for f in flist: 
-			os.system("cp "+condTEMP+" "+dirname+"/Condor.submit"+str(i))
+			os.system("cp "+condTEMP+" "+dirname+"/Condor"+str(i)+".submit")
 			os.system("cp "+wrapTEMP+" "+dirname+"/Warp"+str(i)+".sh")		
-			s1 = open(dirname+"/Condor.submit"+str(i)).read()
-			s1 = s1.replace('@EXESH', dirname+"/Warp"+str(i)+".sh").replace('@LOGS',logsdir).replace('@X509',X509)
-			f1 = open(dirname+"/Condor.submit"+str(i), 'w')
+			s1 = open(dirname+"/Condor"+str(i)+".submit").read()
+			#print textname
+			if ('Run2016C' or 'Run2016E' or 'Run2016G') in str(textname): # run C , E, G takes longer to analyzer 
+				s1 = s1.replace('@EXESH', dirname+"/Warp"+str(i)+".sh").replace('@LOGS',logsdir).replace('@X509',X509).replace('@time','60*60*10')
+			else : 
+				s1 = s1.replace('@EXESH', dirname+"/Warp"+str(i)+".sh").replace('@LOGS',logsdir).replace('@X509',X509).replace('@time','60*60*5')
+			f1 = open(dirname+"/Condor"+str(i)+".submit", 'w')
 			f1.write(s1)
 			f1.close()
 			s2 = open(dirname+"/Warp"+str(i)+".sh").read()
@@ -134,9 +146,10 @@ if __name__ == '__main__':
 			f2.close()
 			file = open('submit_all_to_batch_HTC.sh','a')
 			file.write("\n")
-			file.write("condor_submit "+dirname+"/Condor.submit"+str(i))
+			file.write("condor_submit "+dirname+"/Condor"+str(i)+".submit")
 			file.close() 
 		
 			i+=1
 	os.system('chmod a+x submit_all_to_batch_HTC.sh')
+	print 'submit_all_to_batch_HTC.sh Created for you - you can run it now with ./'
 
